26d00547d1b44b1e6b137611356ee78d8c008fcb
sali_safi <sali_safi@yahoo.com>
2019-01-29 18:28:48 -0500

    ssafi.com resume before html video component

---
+++ asset/js/script.js
@@ -0,0 +1,12 @@
+
+// document.getElementById("experiences").innerHTML = "Hello JavaScript!";
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
