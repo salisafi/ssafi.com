bfd357b577725c69ad9ce5abefbca5e48dad4304
sali_safi <sali_safi@yahoo.com>
2019-01-29 19:51:04 -0500

    small screen size burger button fix

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,15 @@
 
+
+
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 // document.getElementById("experiences").innerHTML = "Hello JavaScript!";
 
 window.onscroll = function() {scrollFunction()};
