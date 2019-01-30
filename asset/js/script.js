4277f281defd97b7cb4d08758e8ab3c271fef16d
sali_safi <sali_safi@yahoo.com>
2019-01-30 08:00:59 -0500

    Fix video - works for all screen sizes

--- asset/js/script.js
+++ asset/js/script.js
@@ -10,6 +10,11 @@ function myFunction() {
   }
 }
 
+
+
+/* document.getElementById("audio").play(); */
+ 
+
 // document.getElementById("experiences").innerHTML = "Hello JavaScript!";
 
 window.onscroll = function() {scrollFunction()};
